eb5b75cfb179c7455df6d2984ff85dbf6c243647
ed.chang <ed.chang@xuenn.com>
2018-04-14 11:32:39 +0800

    Create First Test Case "Test_Rolling_Zero_Pin_Score_Zero"

--- Bowling-Game-Kata/Bowling-Game-Kata.UnitTests/BowlingGameTests.cs
+++ Bowling-Game-Kata/Bowling-Game-Kata.UnitTests/BowlingGameTests.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace BowlingGameKata.UnitTests
 {
+    [TestFixture]
     public class BowlingGameTests
     {
-        
+        [Test]
+        public void Test_Rolling_Zero_Pin_Score_Zero()
+        {
+            // Arrange
+            var expect = 0;
+            var _target = new BowlingGame();
+
+            // Act
+            for (int i = 0; i < 20; i++)
+            {
+                _target.roll(0);
+            }
+            var result = _target.score();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expect));
+        }
     }
 }
---
+++ Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingGameKata
+{
+    public class BowlingGame
+    {
+        public void roll(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object score()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
