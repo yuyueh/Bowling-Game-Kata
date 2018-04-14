5a69f4ab000526590f68232f0f301be61733c6e9
ed.chang <ed.chang@xuenn.com>
2018-04-14 12:06:04 +0800

    Refactor BowlingGame Class

--- Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
+++ Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
@@ -9,7 +9,7 @@ namespace BowlingGameKata
         private int[] _rolls = new int[20];
         private int _currentIndex = 0;
         private int _currentFrame = 0;
-        private int _totalFrame = 10;
+        private int _maxFrame = 10;
 
         public void roll(int pins)
         {
@@ -21,21 +21,36 @@ namespace BowlingGameKata
         {
             int _score = 0;
 
-            for (int i = 0; i < _totalFrame; i++)
+            for (int i = 0; i < _maxFrame; i++)
             {
-                if (_rolls[_currentFrame * 2] + _rolls[_currentFrame * 2 + 1] == 10)
+                if (IsSpare(_currentFrame))
                 {
-                    _score = _score + _rolls[_currentFrame * 2] + _rolls[_currentFrame * 2 + 1] + _rolls[_currentFrame * 2 + 2];
+                    _score += GetFramelScoreWithSpareBonus(_currentFrame);
                 }
                 else
                 {
-                    _score = _score + _rolls[_currentFrame * 2] + _rolls[_currentFrame * 2 + 1];
+                    _score += GetFrameScore(_currentFrame);
                 }
 
                 _currentFrame++;
             }
 
             return _score;
+        }
+
+        private bool IsSpare(int frame)
+        {
+            return _rolls[frame * 2] + _rolls[frame * 2 + 1] == 10;
+        }
+
+        private int GetFrameScore(int frame)
+        {
+            return _rolls[frame * 2] + _rolls[frame * 2 + 1];
+        }
+
+        private int GetFramelScoreWithSpareBonus(int frame)
+        {
+            return _rolls[frame * 2] + _rolls[frame * 2 + 1] + _rolls[frame * 2 + 2];
         }
     }
 }
