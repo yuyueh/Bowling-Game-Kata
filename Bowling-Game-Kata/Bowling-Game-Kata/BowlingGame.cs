1b5291b5253dc8b69f0b3d1c26d27254a115f1f8
ed.chang <ed.chang@xuenn.com>
2018-04-14 11:59:58 +0800

    Implement BowlingGame To Pass Test Case "Test_Rolling_With_Spare"

--- Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
+++ Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
@@ -6,15 +6,35 @@ namespace BowlingGameKata
 {
     public class BowlingGame
     {
-        private int _score = 0;
+        private int[] _rolls = new int[20];
+        private int _currentIndex = 0;
+        private int _currentFrame = 0;
+        private int _totalFrame = 10;
 
         public void roll(int pins)
         {
-            _score += pins;
+            _rolls[_currentIndex] = pins;
+            _currentIndex++;
         }
 
         public int score()
         {
+            int _score = 0;
+
+            for (int i = 0; i < _totalFrame; i++)
+            {
+                if (_rolls[_currentFrame * 2] + _rolls[_currentFrame * 2 + 1] == 10)
+                {
+                    _score = _score + _rolls[_currentFrame * 2] + _rolls[_currentFrame * 2 + 1] + _rolls[_currentFrame * 2 + 2];
+                }
+                else
+                {
+                    _score = _score + _rolls[_currentFrame * 2] + _rolls[_currentFrame * 2 + 1];
+                }
+
+                _currentFrame++;
+            }
+
             return _score;
         }
     }
