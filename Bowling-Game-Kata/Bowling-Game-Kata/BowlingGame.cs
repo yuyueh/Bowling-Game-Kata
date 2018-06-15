3f38426d6ff1f70645a9c6c55cd1fb92712df093
ed chang <ed.chang@xuenn.com>
2018-06-15 11:08:32 +0800

    Implement to pass test case "Test_RollOneSpare"

--- Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
+++ Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
@@ -1,21 +1,41 @@
-﻿namespace BowlingGameKata
+﻿using System.Collections.Generic;
+
+namespace BowlingGameKata
 {
     public class BowlingGame
     {
-        private int _score = 0;
+        private List<int> _rolls;
 
         public BowlingGame()
         {
+            _rolls = new List<int>();
         }
 
         public void Roll(int pins)
         {
-            _score += pins;
+            _rolls.Add(pins);
         }
 
         public int Score()
         {
-            return _score;
+            var result = 0;
+            var currentRoll = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (_rolls[currentRoll] + _rolls[currentRoll + 1] == 10)
+                {
+                    result += 10 + _rolls[currentRoll + 2];
+                    currentRoll += 2;
+                }
+                else
+                {
+                    result += _rolls[currentRoll] + _rolls[currentRoll + 1];
+                    currentRoll += 2;
+                }
+            }
+
+            return result;
         }
     }
 }
