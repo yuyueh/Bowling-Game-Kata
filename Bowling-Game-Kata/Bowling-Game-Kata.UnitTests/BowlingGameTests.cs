1d4a3bfd1bb8397d2cdb87a9117582302a452304
ed chang <ed.chang@xuenn.com>
2018-06-15 10:58:21 +0800

    Create test case "Test_GutterGame" to fail

--- Bowling-Game-Kata/Bowling-Game-Kata.UnitTests/BowlingGameTests.cs
+++ Bowling-Game-Kata/Bowling-Game-Kata.UnitTests/BowlingGameTests.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace BowlingGameKata.UnitTests
 {
     public class BowlingGameTests
     {
-        
+        [Test]
+        public void Test_GutterGame()
+        {
+            // Arrange
+            var target = new BowlingGame();
+            var expected = 0;
+
+            // Act
+            for (int i = 0; i < 20; i++)
+            {
+                target.Roll(0);
+            }
+            var result = target.Score();
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
     }
 }
---
+++ Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
@@ -0,0 +1,19 @@
+﻿namespace BowlingGameKata
+{
+    public class BowlingGame
+    {
+        public BowlingGame()
+        {
+        }
+
+        public void Roll(int i)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Score()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
