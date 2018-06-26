f2513382c0047ffb33119ad432a64aafb0f9a8a4
ed chang <ed.chang@xuenn.com>
2018-06-26 11:27:49 +0800

    Refactor

--- Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
+++ Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
@@ -5,6 +5,7 @@ namespace BowlingGameKata
     public class BowlingGame
     {
         private List<int> _rolls;
+        private readonly int _totalFrames = 10;
 
         public BowlingGame()
         {
@@ -16,26 +17,51 @@ namespace BowlingGameKata
             var score = 0;
             var currentRoll = 0;
 
-            for (int frame = 0; frame < 10; frame++)
+            for (int frame = 0; frame < _totalFrames; frame++)
             {
-                if (_rolls[currentRoll] == 10)
+                if (IsStrike(currentRoll))
                 {
-                    score += 10 + _rolls[currentRoll + 1] + _rolls[currentRoll + 2];
+                    score += 10 + GetStrikeBonus(currentRoll);
                     currentRoll += 1;
                 }
-                else if (_rolls[currentRoll] + _rolls[currentRoll + 1] == 10)
+                else if (IsSpare(currentRoll))
                 {
-                    score += 10 + _rolls[currentRoll + 2];
+                    score += 10 + GetSpareBonus(currentRoll);
                     currentRoll += 2;
                 }
                 else
                 {
-                    score += _rolls[currentRoll] + _rolls[currentRoll + 1];
+                    score += GetNormalScore(currentRoll);
                     currentRoll += 2;
                 }
             }
 
             return score;
+        }
+
+        private int GetNormalScore(int currentRoll)
+        {
+            return _rolls[currentRoll] + _rolls[currentRoll + 1];
+        }
+
+        private int GetSpareBonus(int currentRoll)
+        {
+            return _rolls[currentRoll + 2];
+        }
+
+        private int GetStrikeBonus(int currentRoll)
+        {
+            return _rolls[currentRoll + 1] + _rolls[currentRoll + 2];
+        }
+
+        private bool IsSpare(int currentRoll)
+        {
+            return _rolls[currentRoll] + _rolls[currentRoll + 1] == 10;
+        }
+
+        private bool IsStrike(int currentRoll)
+        {
+            return _rolls[currentRoll] == 10;
         }
 
         public void Roll(int pins)
