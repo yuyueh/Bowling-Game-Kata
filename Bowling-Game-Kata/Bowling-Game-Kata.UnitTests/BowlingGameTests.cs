7900ccaca4493b00755cdd673b1aa6d0b44230b5
ed chang <ed.chang@xuenn.com>
2018-06-26 11:05:12 +0800

    Test_GutterGame

--- Bowling-Game-Kata/Bowling-Game-Kata.UnitTests/BowlingGameTests.cs
+++ Bowling-Game-Kata/Bowling-Game-Kata.UnitTests/BowlingGameTests.cs
@@ -1,7 +1,25 @@
-﻿namespace BowlingGameKata.UnitTests
+﻿using NUnit.Framework;
+
+namespace BowlingGameKata.UnitTests
 {
     public class BowlingGameTests
     {
-        
+        [Test]
+        public void Test_GutterGame()
+        {
+            // Arrange
+            var target = new BowlingGame();
+            var expected = 0;
+
+            // Act
+            for (int i = 0; i < 20; i++)
+            {
+                target.Roll(0);
+            }
+            var result = target.Score();
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
     }
 }
--- Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
+++ Bowling-Game-Kata/Bowling-Game-Kata/BowlingGame.cs
@@ -2,5 +2,14 @@
 {
     public class BowlingGame
     {
+        public int Score()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Roll(int i)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
